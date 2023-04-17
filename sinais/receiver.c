7738a7e01906ca98f2cd5e1f7d8138a290b42c34
Gabriel Ruas <gabrielamaralruas@gmail.com>
2023-04-17 00:18:14 -0300

    parte 1 terminada; sinais enviando e recebendo

---
+++ sinais/receiver.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void term_handler(int signum){
+    printf("Este programa recebeu uma ordem de interrupção. Vamos ser desligados em 3 segundos.\n");
+    sleep(3);
+    exit(0);
+}
+
+void trap_handler(int signum){
+    printf("IT'S A TRAP!, disse Jar Jar Binks.\n");
+}
+
+void alarm_handler(int signum){
+    printf("Recebi um alarme, mas continuo rodando. Sou como seu sono.\n");
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int chosen_type;
+
+    printf("Olá, sou o processo %d.\n", getpid());
+
+    printf("Você deseja que este programa utilize qual estratégia de espera?\nDigite o número associado à modalidade escolhida:\n");
+    printf("1 - Busy wait  #########  2 - Blocking wait\n");
+    scanf("%d", &chosen_type);
+
+    signal(SIGTERM, term_handler);
+    signal(SIGTRAP, trap_handler);
+    signal(SIGALRM, alarm_handler);
+
+    switch (chosen_type)
+    {
+    case 1:
+        while(1==1) {} 
+    case 2:
+        while(1) {
+            pause();
+        }       
+    default:
+        break;
+    }
+
+}
